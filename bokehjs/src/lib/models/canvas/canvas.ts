bskinn/bokeh

Add support for editable Label rotation

bokehjs/src/lib/models/canvas/canvas.ts
@@ -11,6 +11,7 @@ import {type PlotView} from "../plots/plot"
 import type {ReglWrapper} from "../glyphs/webgl/regl_wrap"
 import {StyleSheet, StyleSheetLike} from "core/dom"
 import canvas_css from "styles/canvas.css"
+import icons_css from "styles/icons.css"
 
 export type FrameBox = [number, number, number, number]
 
@@ -110,7 +111,7 @@ export class CanvasView extends UIElementView {
   }
 
   override styles(): StyleSheetLike[] {
-    return [...super.styles(), canvas_css, this._size]
+    return [...super.styles(), canvas_css, icons_css, this._size]
   }
 
   override render(): void {

Jonisliuqiang/bokeh-bokeh

bokehjs/src/lib/models/canvas/canvas.ts
@@ -13,6 +13,7 @@ import type {ReglWrapper} from "../glyphs/webgl/regl_wrap"
 import type {StyleSheetLike} from "core/dom"
 import {InlineStyleSheet} from "core/dom"
 import canvas_css from "styles/canvas.css"
+import icons_css from "styles/icons.css"
 
 export type FrameBox = [number, number, number, number]
 
@@ -126,7 +127,7 @@ export class CanvasView extends UIElementView {
   }
 
   override stylesheets(): StyleSheetLike[] {
-    return [...super.stylesheets(), canvas_css, this._size]
+    return [...super.stylesheets(), canvas_css, icons_css, this._size]
   }
 
   override render(): void {